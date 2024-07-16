aca2a6481bdfed986d44f558fa0e8284c0aba88f
sparshatandukar <tandukarsparsha@gmail.com>
2024-07-16 22:21:40 +0545

    brew commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors:
+      {
+        browninsh: '#5c4938',
+        lightGrey: '#d9d9d8',
+        yellowish: '#bd9643'
+      }
+    },
+  },
+  plugins: [],
+}
+
